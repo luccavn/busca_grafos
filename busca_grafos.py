dcb84b046f75d85f4c40883c41980d7d70c9dd59
Lucca Vinicius <luccavinicius.inbox@gmail.com>
2019-03-14 03:06:59 -0300

    Refactored all classes and methods due to good practices.

--- busca_grafos.py
+++ busca_grafos.py
@@ -15,15 +15,11 @@ FONT_SIZE = 11              # Tamanho da fonte.
 DOT_RADIUS = 4              # Raio dos pontos do mapa.
 LINE_WIDTH = 2              # Espessura das linhas de rota.
 
-ROOT_DIR = path.dirname(path.abspath(__file__)) # Diretório raiz do projeto.
-
-# Carrega a imagem do mapa.
-map_path = path.join(ROOT_DIR, 'mapa_vale.png')
-with pygame.image.load(map_path) as map_image:
-    vale_map_img = pygame.transform.scale(map_image), SCREEN_SIZE)
+root_dir = path.dirname(path.abspath(__file__)) # Diretório raiz do projeto.
+map_path = path.join(root_dir, 'mapa_vale.png') # Diretório da imagem do mapa.
 
 pygame.init()   # Inicializa os módulos do pygame.
-pygame.display.set_caption('Projeto Vale do Paraíba')   # Altera o título da janela.
+pygame.display.set_caption('Algoritmos de Busca em Grafo - Vale do Paraíba')   # Altera o título da janela.
 
 screen = pygame.display.set_mode(SCREEN_SIZE)    # Altera as dimensões da janela.
 
@@ -31,7 +27,10 @@ sys_font = pygame.font.SysFont('Comic Sans MS', FONT_SIZE) # Carrega uma fonte e
 title_font = pygame.font.SysFont('Comic Sans MS', FONT_SIZE+10) # Carrega uma fonte específica do sistema para uso.
 medium_font = pygame.font.SysFont('Comic Sans MS', FONT_SIZE+2)
 
-@class
+# Carrega a imagem do mapa.
+map_image = pygame.image.load(map_path)
+map_image_scaled = pygame.transform.scale(map_image, SCREEN_SIZE)
+
 class Vertex:
     """ Representa um vértice do grafo 
 
@@ -67,7 +66,6 @@ class Vertex:
     def add_neighbour(self, neighbour):
         self._neighbours.add(neighbour)
 
-@class
 class County():
     """ Representa um município no mapa 
     
@@ -100,7 +98,7 @@ def already_exists(name, lst):
 
     Se o objeto do tipo Node não existir na lista, retorna None."""
     for node in lst:
-        if node.get_name() == name:
+        if node.name == name:
             return node
     return None
 
@@ -112,7 +110,7 @@ def get_city_byname(cities, name):
     - cities : lista de municípios.
     - name : nome do município desejado."""
     for city in cities:
-        if city.get_name() == name:
+        if city.name == name:
             return city
     return None
 
@@ -143,7 +141,7 @@ def dijkstra(edges, f, t):
 if __name__ == "__main__":
 
     # Faz a leitura dos dados contidos no arquivo dist_vale.csv e cria uma tabela.
-    data = pd.read_csv(path.join(ROOT_DIR, 'dist_vale.csv'), sep=',', encoding='utf-8')
+    data = pd.read_csv(path.join(root_dir, 'dist_vale.csv'), sep=',', encoding='utf-8')
     values = data.values    # Transforma os valores da tabela em uma matriz.
 
     # Varre a matriz e cria uma lista de Vértices.
@@ -165,15 +163,15 @@ if __name__ == "__main__":
             new_vertex.add_neighbour(new_neighbour)
             vertexes.append(new_vertex)
 
-    # Varre a lista de Nodes e cria a lista de Vértices para uso no algoritmo de Dijkstra.
+    # Varre a lista de Vértices e cria uma lista traduzida para uso no algoritmo de Dijkstra.
     edges = list()
-    for node in nodes:
-        for neighbour in node.neighbours:
-            new_edge = (node.get_name(), neighbour.get_name(), neighbour.get_distance())
+    for vertex in vertexes:
+        for neighbour in vertex.neighbours:
+            new_edge = (vertex.name, neighbour.name, neighbour.distance)
             edges.append(new_edge)
 
     # Faz a leitura das posições dos pontos no mapa, contidos no arquivo map_dots.csv
-    map_data = pd.read_csv(path.join(ROOT_DIR, 'map_dots.csv'), sep=',', encoding='utf-8')
+    map_data = pd.read_csv(path.join(root_dir, 'map_dots.csv'), sep=',', encoding='utf-8')
     map_values = map_data.values    # Transforma os dados em uma matriz.
 
     # Cria os MapDots que representam os pontos no mapa.
@@ -201,12 +199,12 @@ if __name__ == "__main__":
             if event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:   # Botão direito do mouse determina o município de origem do mapa.
                     for city in map_cities:
-                        if city.get_rect().colliderect(mouse_rect): # Verifica se o retângulo do ponteiro colide com o retângulo do ponto no mapa.
-                            from_city = city.get_name()
+                        if city.rect.colliderect(mouse_rect): # Verifica se o retângulo do ponteiro colide com o retângulo do ponto no mapa.
+                            from_city = city.name
                 elif event.button == 3: # Botão esquerdo do mouse determina o município de destino do mapa.
                     for city in map_cities:
-                        if city.get_rect().colliderect(mouse_rect):
-                            to_city = city.get_name()
+                        if city.rect.colliderect(mouse_rect):
+                            to_city = city.name
                 if from_city and to_city:   # Traça a rota entre os municípios escolhidos no mapa.
                     path = dijkstra(edges, from_city, to_city)
                     route_dist = path[0]
@@ -216,16 +214,16 @@ if __name__ == "__main__":
                     print('\nRota: {} ate {}\nDistancia: aprox. {} km.\nPercurso: {}\n'.format(from_city, to_city, ceil(route_dist), route_path))
 
         screen.fill(COLOR_WHITE)    # Preenche o fundo com a cor específica.
-        screen.blit(vale_map_img, (0, 0))   # Desenha a imagem do mapa.
+        screen.blit(map_image_scaled, (0, 0))   # Desenha a imagem do mapa.
 
         for city in map_cities: # Varre a lista de municípios.
-            pygame.draw.circle(screen, COLOR_BLACK, city.get_pos(), DOT_RADIUS) # Desenha o ponto do município no mapa.
-            name_text = medium_font.render(city.get_name(), True, COLOR_BLACK)    # Cria o texto que representa o nome do município.
-            screen.blit(name_text, (city.get_pos()[0]-len(city.get_name())*3, city.get_pos()[1]))   # Desenha o nome do município no mapa.
+            pygame.draw.circle(screen, COLOR_BLACK, city.pos, DOT_RADIUS) # Desenha o ponto do município no mapa.
+            name_text = medium_font.render(city.name, True, COLOR_BLACK)    # Cria o texto que representa o nome do município.
+            screen.blit(name_text, (city.pos[0]-len(city.name)*3, city.pos[1]))   # Desenha o nome do município no mapa.
 
         if route_path:    # Desenha as linhas da rota atual.
             for i in range(len(route_path)-1):
-                pygame.draw.line(screen, COLOR_BLACK, get_city_byname(map_cities, route_path[i]).get_pos(), get_city_byname(map_cities, route_path[i+1]).get_pos(), LINE_WIDTH)
+                pygame.draw.line(screen, COLOR_BLACK, get_city_byname(map_cities, route_path[i]).pos, get_city_byname(map_cities, route_path[i+1]).pos, LINE_WIDTH)
                 title_text = title_font.render('Rota: {} até {}, Distância: aprox. {} km.'.format(from_city, to_city, ceil(route_dist)), True, COLOR_BLACK)
                 screen.blit(title_text, (SCREEN_SIZE[0]/4 - 70, 0))
 
